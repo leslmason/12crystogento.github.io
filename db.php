5c81e6f38577f86676eb98c49835b44dfbb6ae64
leslmason <charlescharleslmason@gmail.com>
2023-03-25 15:53:40 +0530

    12crystogento website

---
+++ db.php
@@ -0,0 +1,31 @@
+<?php 
+
+/*$cpassword = $_POST['cpassword']; 
+    
+$mysqli = new mysqli("localhost","intotixg_intor123","Askme@123","intotixg_intorxyz");
+
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+// Turn autocommit off
+$mysqli -> autocommit(FALSE);
+
+// Insert some values
+$mysqli -> query("INSERT INTO tb1 (email,pw,cpassword)
+VALUES ('$email','$pw','$cpassword')");
+
+
+// Commit transaction
+if (!$mysqli -> commit()) {
+  echo "Error: " . $sql . "<br>" . $mysqli->error;
+  exit();
+}
+
+$mysqli -> close();*/
+?>
+<script language="javascript">
+	alert("Thank you for contacting us");
+	window.location="http://12crystogento.net"
+</script>
